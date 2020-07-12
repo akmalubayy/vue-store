eb07cba856b705952a3a06754912c81a3c118cce
akmalubayy <mhmdakmal46@gmail.com>
2020-07-12 17:18:11 +0700

    Convert HTML Front end to Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <HeroProduk />
+    <InstaGallery />
+    <Partners />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Hero from '@/components/Hero.vue';
+import HeroProduk from '@/components/HeroProduk.vue';
+import InstaGallery from '@/components/InstaGallery.vue';
+import Partners from '@/components/Partners.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    HeroProduk,
+    InstaGallery,
+    Partners,
+    Footer,
+  },
+};
+</script>
